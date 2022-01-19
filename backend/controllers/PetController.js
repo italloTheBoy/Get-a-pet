82fd52df71c6fad7736faffdb84f8d32a6e261cd
itallo <itallomkspop@gmail.com>
2022-01-18 21:49:02 -0300

    get conclusion && delete

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -4,42 +4,6 @@ const User = require('../models/User')
 
 
 class PetController {
-  
-  static async getAll(req, res) {
-    
-    try {
-      const pets = await Pet.find().sort('-createdAt')
-      
-      return res.status(200).json({ pets })
-    }
-    catch (err) {
-      console.log(err)
-
-      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
-    }
-
-  }
-
-
-  static async getMy(req, res) {
-
-    const userId = req.userId
-
-    try {
-      const user = await User.findById(userId).select('_id')
-
-      const pets = await Pet.find({ 'user._id': user._id })
-
-      res.status(200).json({ pets })
-    }
-    catch (err) {
-      console.log(err)
-
-      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
-    }
-
-  }
-
 
   static async add(req, res) {
 
@@ -113,6 +77,117 @@ class PetController {
     return res.status(201).json({ message: 'Pet registrado', pet })
         
   }
+  
+  static async getAll(req, res) {
+    
+    try {
+      const pets = await Pet.find().sort('-createdAt')
+      
+      return res.status(200).json({ pets })
+    }
+    catch (err) {
+      console.log(err)
+
+      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
+    }
+
+  }
+
+  static async getById(req, res) {
+
+    const { id } = req.params
+
+
+    try {
+      const pet = await Pet.findById(id)
+
+      if (!pet) {
+        return res.status(406).json({ message: 'Pet não encontrado' })
+      }
+
+      res.status(200).json({ pet })
+    }
+    catch (err) {
+      console.log(err)
+
+      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
+    }
+  }
+
+
+  static async toAdopt(req, res) {
+
+    const userId = req.userId
+
+    try {
+      const user = await User.findById(userId).select('_id')
+
+      const pets = await Pet.find({ 'user._id': user._id })
+
+      res.status(200).json({ pets })
+    }
+    catch (err) {
+      console.log(err)
+
+      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
+    }
+
+  }
+
+
+  static async adopted(req, res) {
+   
+    const userId = req.userId
+
+    try {
+      const user = await User.findById(userId).select('_id')
+
+      const pets = await Pet.find({ 'adopter._id': user._id })
+
+      res.status(200).json({ pets })
+    }
+    catch (err) {
+      console.log(err)
+
+      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
+    }
+ 
+  }
+
+  static async delete(req, res) {
+    const userId = req.userId
+    const { id } = req.params
+
+    try {
+      const user = await User.findById(userId)
+
+      if (!user) {
+        return res.status(401).json({ message: 'Acesso negado'})
+      }
+
+
+      const deletedPet = await Pet.findOneAndDelete({ 
+        _id: id,
+        'user._id': user._id
+      })
+
+      if (!deletedPet) {
+        return res.status(406).json({ message: 'Pet não encontrado'})
+      }
+
+
+      return res.status(200).json({ message: 'Pet deletado'})
+      
+    }
+    catch (err) {
+      console.log(err)
+
+      return res.status(500).json({ message: 'Ocorreu um erro inesperado'})
+    }
+
+    
+  }
+  
 }
 
 
--- backend/routers/petRouter.js
+++ backend/routers/petRouter.js
@@ -7,7 +7,11 @@ const PetController = require('../controllers/PetController')
 router.post('/register', check, imageUpload.array('images'), PetController.add)
 
 router.get('/', PetController.getAll)
-router.get('/mypets', check, PetController.getMy)
+router.get('/:id', PetController.getById)
+router.get('/my/toAdopt', check, PetController.toAdopt)
+router.get('/my/adopted', check, PetController.adopted)
+
+router.delete('/:id', check, PetController.delete)
 
 
 module.exports = router
